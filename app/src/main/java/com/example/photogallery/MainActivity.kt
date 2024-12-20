34496211a35b2ea864ccdba0213a917cb1bb0133
alyavit <alyavit1@gmail.com>
2024-12-20 18:31:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.photogallery.ui.view
+
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.photogallery.R
+import com.example.photogallery.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+}
